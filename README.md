ffea8ab5c522147806dc0c9e3ce8b017e0067a71
Doth-J <theodjoan@gmail.com>
2025-01-15 03:18:40 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cosmic Journey
+
+Vast new galaxies expand in the infinite cosmos. We are but a spec of time in the sea of the moments since the begining of time.
---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import Lamp from "./components/Lamp";
+import useTitle from "./hooks/useTitle";
+import Portrait from "./components/Portrait";
+import Creator from "./components/Creator";
+import AudioPlayer from "./components/Audio";
+
+export default function App({ title }: { title: string }) {
+  useTitle(title);
+  const [isOpen, setOpen] = useState<boolean>(true);
+
+  return (
+    <div className={`display`}>
+      <Lamp isOpen={isOpen} width={625} setOpen={setOpen} />
+      <Portrait isOpen={isOpen} title={title}>
+        <div className={`container ${isOpen ? "on" : "off"}`}>
+          {" "}
+          <AudioPlayer isOpen={!isOpen} source="./whispers.mp3" />
+        </div>
+      </Portrait>
+      <Creator
+        isOpen={isOpen}
+        creator="Developed by Doth-J"
+        link="https://github.com/Doth-J"
+      >
+        <AudioPlayer isOpen={!isOpen} source="./voyage.mp3" />
+      </Creator>
+    </div>
+  );
+}
